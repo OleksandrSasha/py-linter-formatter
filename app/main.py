93df2d215e00ff9373609ac04720722b8b7a79f1
Oleksandr Kozlov <aleksandrkozlov2015@gmail.com>
2024-08-06 02:18:46 +0200

    New solution

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ errors = [
         "line_number": 18,
         "column_number": 80,
         "text": "line too long (99 > 79 characters)",
-        "physical_line": 'return f"I like to filter, rounding, doubling,' 
+        "physical_line": 'return f"I like to filter, rounding, doubling,'
         "store and decorate numbers: {', '.join(items)}!\"",
     },
     {
@@ -69,7 +69,7 @@ report_file = {
                 "line_number": 18,
                 "column_number": 80,
                 "text": "line too long (99 > 79 characters)",
-                "physical_line": 'return f"I like to filter, rounding, doubling,'
+                "physical_line": 'return f"I like to filter,rounding,doubling,'
                 "store and decorate numbers: {', '.join(items)}!\"",
             },
             {
@@ -78,10 +78,10 @@ report_file = {
                 "line_number": 18,
                 "column_number": 100,
                 "text": "no newline at end of file",
-                "physical_line": 'return f"I like to filter, rounding, doubling,'
+                "physical_line": 'return f"I like to filter,rounding,doubling,'
                 "store and decorate numbers: {', '.join(items)}!\"",
             }
-        ]
+          ]
 }
 
 print(format_linter_report(linter_report=report_file))
